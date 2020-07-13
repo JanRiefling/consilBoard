f6726a9ae177f5c2e08c3fe114fda1d57f1122b4
JanRiefling <jan.riefling@web.de>
2020-07-13 15:15:37 +0200

    Set up project

---
+++ consilboard-backend/src/main/java/de/neuefische/consilboard/ConsilboardApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.consilboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsilboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsilboardApplication.class, args);
+    }
+
+}
---
+++ consilboard-backend/src/test/java/de/neuefische/consilboard/ConsilboardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.consilboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsilboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
