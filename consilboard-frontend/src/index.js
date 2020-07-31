2314937a850780bf514b0d0f092f20439da3f601
JanRiefling <jan.riefling@web.de>
2020-07-31 09:34:16 +0200

    Theme Provider

--- consilboard-frontend/src/index.js
+++ consilboard-frontend/src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <MuiThemeProvider theme={myTheme}>
+        <App />
+      </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
